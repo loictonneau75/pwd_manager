24dbd3384c567339e1f1246c22fa67821bab19a5
loictonneau75 <loictonneau75@gmail.com>
2023-04-27 19:58:59 +0200

    sortie de la classe appselector dans son propre fichier

---
+++ ui/application_selector/application_selector.py
@@ -0,0 +1,68 @@
+import tkinter as tk
+from tkinter import ttk
+from ui.account_creator.account_creator import AccountCreator
+from ui.account_finder.account_finder import AccountFinder
+
+class AppChoice(ttk.Labelframe):
+    """
+    A class representing the app choice label frame.
+
+    Attributes:
+        master (tk.Tk): The master window.
+    
+    Methods:
+        create_widget() : Creates the widgets for the AppChoice object.
+        create_new_button() : Creates the 'Creer un compte' button and assigns its command.
+        create_get_button() : Creates the 'Afficher un compte' button and assigns its command.
+        switch_app() : Switches to a new application.
+        return_to_appchoice() : Returns to the AppChoice screen.
+    """
+
+    def __init__(self, master: tk.Tk):
+        """
+        Initializes the AppChoice object.
+
+        Args:
+            master (tk.Tk): The master window.
+        """
+        super().__init__(master, text="Choix de l'application")
+        self.create_widget()
+        self.pack()
+
+    def create_widget(self):
+        """
+        Creates the widgets for the AppChoice object.
+        """
+        self.create_new_button()
+        self.create_get_button()
+
+    def create_new_button(self):
+        """
+        Creates the 'Creer un compte' button and assigns its command.
+        """
+        self.create_button = ttk.Button(self, text="Creer un compte")
+        self.create_button.configure(command = lambda : self.switch_app(AccountCreator))
+        self.create_button.pack(padx=20, pady=(20, 5))
+
+    def create_get_button(self):
+        """
+        Creates the 'Afficher un compte' button and assigns its command.
+        """
+        self.get_button = ttk.Button(self, text="Afficher un compte")
+        self.get_button.configure(command =  lambda : self.switch_app(AccountFinder))
+        self.get_button.pack(padx=20, pady=(0, 20))
+
+    def switch_app(self, new_app):
+        """
+        Switches to a new application.
+
+        Args:
+            new_app: The new application to be displayed.
+        """
+        self.master.master.change_app(new_app, text_exit_button = "Retour", command = self.return_to_appchoice)
+
+    def return_to_appchoice(self):
+        """
+        Returns to the AppChoice screen.
+        """
+        self.master.master.change_app(AppChoice, text_exit_button = "Quitter", command = self.master.master.destroy)
