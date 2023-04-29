72f92ae9181df4a43f1a1f67c241866e05f70764
loictonneau75 <loictonneau75@gmail.com>
2023-04-29 16:48:01 +0200

    creation de la methode check company lenght et passage de la variable start_row en attribut

--- ui/account_creator/account_creator.py
+++ ui/account_creator/account_creator.py
@@ -6,16 +6,20 @@ from ui.account_creator.engine import *
 
 class AccountCreator(ttk.Labelframe):
 
-    def __init__(self, master: tk.Tk):
+    def __init__(self, master: ttk.Frame):
         super().__init__(master, text = "Création de compte")
         self.create_widget()
+        # self.erase_error()
+        # self.config_entry()
+        self.company.error_no_company.grid_forget()
+        self.company.value.trace("w", self.company.check_company_entry_lenght())
         self.pack()
 
     def create_widget(self):
         self.company = Company(self,0)
-        self.email = Email(self, self.winfo_children()[-1].grid_info()["row"])
-        self.pseudo = Pseudo(self, self.winfo_children()[-1].grid_info()["row"])
-        self.password = Password(self, self.winfo_children()[-1].grid_info()["row"])
+        self.email = Email(self, self.winfo_children()[-1].grid_info()["row"] + 1)
+        self.pseudo = Pseudo(self, self.winfo_children()[-1].grid_info()["row"] + 1)
+        self.password = Password(self, self.winfo_children()[-1].grid_info()["row"] + 1)
         self.create_valid_button()
 
     def create_valid_button(self):
@@ -30,59 +34,72 @@ class AccountCreator(ttk.Labelframe):
 class Company():
 
     def __init__(self, master, start_row):
-        self.create_widget(master,start_row)
+        self.start_row = start_row
+        self.create_widget(master)
 
-    def  create_widget(self, master, start_row):
-        self.value_var = tk.StringVar()
-        self.label = create_label(master, "Nom de la compagnie :", (start_row, 0))
-        self.entry = create_entry(master, self.value_var, (start_row, 1))
-        self.error_no_company = create_error(master, "Veuillez mettre le nom de la compagnie pour laquelle vous enregistrez un compte", (start_row+1, 0))
+    def  create_widget(self, master):
+        self.value = tk.StringVar()
+        self.label = create_label(master, "Nom de la compagnie :", (self.start_row, 0))
+        self.entry = create_entry(master, self.value, (self.start_row, 1))
+        self.error_no_company = create_error(master, "Veuillez mettre le nom de la compagnie pour laquelle vous enregistrez un compte", (self.start_row + 1, 0))
+
+    def check_company_entry_lenght(self):
+        def _check_company(*args):
+            value = self.value.get()
+            if self.value.get() == "":
+                self.error_no_company.grid(row = self.start_row + 1, column = 0, columnspan = 2)
+            else:
+                self.error_no_company.grid_forget()
+        return _check_company
 
 
 class Email():
 
     def __init__(self, master, start_row):
-        self.create_widget(master, start_row)
+        self.start_row = start_row
+        self.create_widget(master)
 
-    def  create_widget(self,master,start_row):
+    def  create_widget(self,master):
         self.value = tk.StringVar()
-        self.label = create_label(master, "Email :", (start_row, 0))
-        self.entry = create_entry(master, self.value, (start_row, 1))
-        self.error_no_email = create_error(master, "Veuillez mettre l'adresse utilisé pour créer un compte", (start_row + 1, 0))
-        self.error_incorect_email = create_error(master, "Email incorrecte", (start_row + 2, 0) )
+        self.label = create_label(master, "Email :", (self.start_row, 0))
+        self.entry = create_entry(master, self.value, (self.start_row, 1))
+        self.error_no_email = create_error(master, "Veuillez mettre l'adresse utilisé pour créer un compte", (self.start_row + 1, 0))
+        self.error_incorect_email = create_error(master, "Email incorrecte", (self.start_row + 2, 0) )
 
 
 class Pseudo():
 
     def __init__(self, master, start_row):
-        self.create_widget(master, start_row)
+        self.start_row = start_row
+        self.create_widget(master)
 
-    def  create_widget(self, master, start_row):
+    def  create_widget(self, master):
         self.is_necessary = tk.BooleanVar()
         self.is_necessary_checkbox = create_checkbox(master, self.is_necessary, "Avez vous besoin d'un pseudo", 
-                                                     lambda: print(self.is_necessary.get()), (start_row, 0))
+                                                     lambda: print(self.is_necessary.get()), (self.start_row, 0))
         self.is_already_possessed = tk.BooleanVar()
         self.is_already_possessed_checkbox = create_checkbox(master, self.is_already_possessed, "Avez vous deja un pseudo",
-                                                             lambda: print(self.is_already_possessed.get()), (start_row, 1))
+                                                             lambda: print(self.is_already_possessed.get()), (self.start_row, 1))
         self.value = tk.StringVar()
-        self.label = create_label(master, "Pseudo :", (start_row + 1, 0))
-        self.entry = create_entry(master, self.value, (start_row + 1, 1))
-        self.error_no_pseudo = create_error(master,"Veuillez renseigné un pseudo",(start_row + 2, 0))
+        self.label = create_label(master, "Pseudo :", (self.start_row + 1, 0))
+        self.entry = create_entry(master, self.value, (self.start_row + 1, 1))
+        self.error_no_pseudo = create_error(master,"Veuillez renseigné un pseudo",(self.start_row + 2, 0))
 
 
 class Password():
 
     def __init__(self, master, start_row):
-        self.create_widget(master, start_row)
+        self.start_row = start_row
+        self.create_widget(master)
 
-    def  create_widget(self, master, start_row):
+    def  create_widget(self, master):
         self.is_already_possessed = tk.BooleanVar()
         self.is_already_possessed_checkbox = create_checkbox(master, self.is_already_possessed, "Avez vous deja un mot de passe",
-                                                             lambda: print(self.is_already_possessed.get()), (start_row, 0))
+                                                             lambda: print(self.is_already_possessed.get()), (self.start_row, 0))
         self.value = tk.StringVar()
-        self.label = create_label(master, "Mot de passe :", (start_row + 1, 0))
-        self.entry = create_entry(master, self.value, (start_row + 1, 1))
-        self.error_no_pseudo = create_error(master,"Veuillez renseigné un mot de passe",(start_row + 2, 0))
-        self.label = create_label(master, "Répéter le mot de passe :", (start_row + 3, 0))
-        self.entry = create_entry(master, self.value, (start_row + 3, 1))
-        self.error_no_pseudo = create_error(master,"Les mots de passe doivent corespondre",(start_row + 4, 0))
+        self.label = create_label(master, "Mot de passe :", (self.start_row + 1, 0))
+        self.entry = create_entry(master, self.value, (self.start_row + 1, 1))
+        self.error_no_pseudo = create_error(master,"Veuillez renseigné un mot de passe",(self.start_row + 2, 0))
+        self.label = create_label(master, "Répéter le mot de passe :", (self.start_row + 3, 0))
+        self.entry = create_entry(master, self.value, (self.start_row + 3, 1))
+        self.error_no_pseudo = create_error(master,"Les mots de passe doivent corespondre",(self.start_row + 4, 0))
