6a6a107066bfd71ada8c36b2fa477138245d6e8f
loictonneau75 <loictonneau75@gmail.com>
2023-04-29 12:12:42 +0200

    creation de la partie email et debut de la partie pseudo

--- ui/account_creator/account_creator.py
+++ ui/account_creator/account_creator.py
@@ -1,59 +1,62 @@
 import tkinter as tk
 from tkinter import ttk
 
+from ui.account_creator.engine import *
+
 class AccountCreator(ttk.Labelframe):
 
     def __init__(self, master:tk.Tk):
         super().__init__(master, text = "Création de compte")
-        self.company = Company(self)
-        self.email = Email(self)
-        # self.pseudo = Pseudo(self)
-        # self.password = Password(self)
-
-        self.pack() 
-
-
-class Company(ttk.Frame):
-
-    def __init__(self, master):
-        super().__init__(master)
-        self.create_widget()
-        self.pack(fill = "x", expand = True)
-
-    def  create_widget(self):
-        self.create_label("Nom de la compagnie")
-        self.create_entry()
-        self.create_error("Veuillez mettre le nom de la compagnie pour laquelle vous enregistrez un compte",1)
-        
-    def create_label(self,text):
-        self.label = ttk.Label(self, text = text)
-        self.label.grid(row = 0, column = 0)
-
-    def create_entry(self):
-        self.entry = ttk.Entry(self)
-        self.entry.grid(row = 0, column = 1, sticky = "ew")
-
-    def create_error(self, text, row):
-        self.error_label = ttk.Label(self, text = text, foreground = "red")
-        self.error_label.grid(row = row, column = 0, columnspan = 2)
+        self.company = Company(self,len(self.children))
+        self.email = Email(self,len(self.children))
+        self.pseudo = Pseudo(self,len(self.children))
+        # self.password = Password(self,len(self.children))
+        self.pack()
 
 
-class Email(ttk.Frame):
+class Company():
 
-    def __init__(self, master):
-        super().__init__(master)
-        self.pack(fill="x")
+    def __init__(self, master, start_row):
+        self.create_widget(master,start_row)
+
+    def  create_widget(self,master,start_row):
+        self.value_var = tk.StringVar()
+        self.label = create_label(master,"Nom de la compagnie :",(start_row,0))
+        self.entry = create_entry(master,(start_row,1), self.value_var)
+        self.error_no_company = create_error(master, "Veuillez mettre le nom de la compagnie pour laquelle vous enregistrez un compte",(start_row+1,0))
+    
+
+class Email():
+
+    def __init__(self, master, start_row):
+        self.create_widget(master, start_row)
+
+    def  create_widget(self,master,start_row):
+        self.value_var = tk.StringVar()
+        self.label = create_label(master,"Email :",(start_row,0))
+        self.entry = create_entry(master,(start_row,1), self.value_var)
+        self.error_no_email = create_error(master, "Veuillez mettre l'adresse utilisé pour créer un compte",(start_row+1,0))
+        self.error_incorect_email = create_error(master, "Email incorrecte",(start_row+2,0) )
 
 
-class Pseudo(ttk.Frame):
+class Pseudo():
 
-    def __init__(self, master):
-        super().__init__(master)
-        self.pack(fill = "x")
+    def __init__(self, master, start_row):
+        self.create_widget(master,start_row)
+
+    def  create_widget(self,master,start_row):
+        self.is_necessary_var = tk.BooleanVar()
+        self.is_necessary_checkbox = create_checkbox(master,self.is_necessary_var,"Avez vous besoin d'un pseudo",lambda: print(self.is_necessary_var.get()),(start_row,0))
+
+    def get_value(self):
+        if self.is_necessary_var.get():
+            pass
 
 
+class Password():
 
-class Password(ttk.Frame):
+    def __init__(self, master, start_row):
+        self.create_widget(master,start_row)
 
-    def __init__(self, master):
-        super().__init__(master)
+    def  create_widget(self,master,start_row):
+        pass
---
+++ ui/account_creator/engine.py
@@ -0,0 +1,66 @@
+import tkinter as tk
+from tkinter import ttk
+
+def create_error(master : ttk.Labelframe, text : str, position : tuple):
+    """
+    Creates a label with the given error message and adds it to the given master widget at the specified position.
+
+    Args:
+        master: A ttk.Labelframe widget to which the error label will be added.
+        text: A string containing the error message to be displayed on the label.
+        position: A tuple representing the row and column position of the label in the grid.
+
+    Returns:
+        The ttk.Label widget that was created.
+    """
+    error_label = ttk.Label(master, text = text, foreground = "red")
+    error_label.grid(row = position[0], column = position[1], columnspan = 2, sticky = "ew")
+    return error_label
+
+def create_label(master : ttk.Labelframe, text : str, position : tuple):
+    """
+    Creates a label with the given text and adds it to the given master widget at the specified position.
+
+    Args:
+        master: A ttk.Labelframe widget to which the label will be added.
+        text: A string containing the text to be displayed on the label.
+        position: A tuple representing the row and column position of the label in the grid.
+
+    Returns:
+        The ttk.Label widget that was created.
+    """
+    label = ttk.Label(master, text = text)
+    label.grid(row = position[0], column = position[1], sticky = "ew")
+    return label
+
+def create_entry(master : ttk.Labelframe, position : tuple, variable : tk.StringVar):
+    """
+    Creates an entry widget with the given StringVar and adds it to the given master widget at the specified position.
+
+    Args:
+        master: A ttk.Labelframe widget to which the entry will be added.
+        position: A tuple representing the row and column position of the entry in the grid.
+        variable: A tk.StringVar object to which the entry will be linked.
+
+    Returns:
+        The ttk.Entry widget that was created.
+    """
+    entry = ttk.Entry(master, textvariable = variable)
+    entry.grid(row = position[0], column = position[1], sticky = "ew")
+    return entry
+
+def create_checkbox(master : ttk.Labelframe, variable : tk.BooleanVar, text : str, command, position : tuple):
+    """
+    Creates an entry widget with the given StringVar and adds it to the given master widget at the specified position.
+
+    Args:
+        master: A ttk.Labelframe widget to which the entry will be added.
+        position: A tuple representing the row and column position of the entry in the grid.
+        variable: A tk.StringVar object to which the entry will be linked.
+
+    Returns:
+        The ttk.Entry widget that was created.
+    """
+    check_box = ttk.Checkbutton(master, text = text, command = command, variable = variable)
+    check_box.grid(row = position[0], column = position[1], sticky = "ew")
+    return check_box
