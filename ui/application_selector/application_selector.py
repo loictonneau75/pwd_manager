c5f65fe50b99b759ca47bb49a9806d25b69f31ff
loictonneau75 <loictonneau75@gmail.com>
2023-04-30 17:11:59 +0200

    formatage

--- ui/application_selector/application_selector.py
+++ ui/application_selector/application_selector.py
@@ -18,13 +18,14 @@ class AppChoice(ttk.Labelframe):
         return_to_appchoice() : Returns to the AppChoice screen.
     """
 
-    def __init__(self, master: tk.Tk) -> None:
+    def __init__(self, master: tk.Frame) -> None:
         """
         Initializes the AppChoice object.
 
         Args:
             master (tk.Tk): The master window.
         """
+        print(master)
         super().__init__(master, text="Choix de l'application")
         self.create_widget()
         self.pack()
@@ -40,17 +41,17 @@ class AppChoice(ttk.Labelframe):
         """
         Creates the 'Creer un compte' button and assigns its command.
         """
-        self.create_button = ttk.Button(self, text="Creer un compte")
-        self.create_button.configure(command = lambda : self.switch_app(AccountCreator))
-        self.create_button.pack(padx=20, pady=(20, 5))
+        self.create_button = ttk.Button(self, text = "Creer un compte")
+        self.create_button.configure(command = lambda: self.switch_app(AccountCreator))
+        self.create_button.pack(padx = 20, pady = (20, 5))
 
     def create_get_button(self) -> None:
         """
         Creates the 'Afficher un compte' button and assigns its command.
         """
-        self.get_button = ttk.Button(self, text="Afficher un compte")
-        self.get_button.configure(command =  lambda : self.switch_app(AccountFinder))
-        self.get_button.pack(padx=20, pady=(0, 20))
+        self.get_button = ttk.Button(self, text = "Afficher un compte")
+        self.get_button.configure(command = lambda: self.switch_app(AccountFinder))
+        self.get_button.pack(padx = 20, pady = (0, 20))
 
     def switch_app(self, next_application = None) -> None:
         """
@@ -63,12 +64,12 @@ class AppChoice(ttk.Labelframe):
         if next_application:
             main_window.change_app(
                 next_application,
-                exit_button_text="Retour",
-                exit_button_command=self.switch_app
+                exit_button_text = "Retour",
+                exit_button_command = self.switch_app
             )
         else:
             main_window.change_app(
                 AppChoice,
-                exit_button_text="Quitter",
-                exit_button_command=main_window.destroy
+                exit_button_text = "Quitter",
+                exit_button_command = main_window.destroy
             )
